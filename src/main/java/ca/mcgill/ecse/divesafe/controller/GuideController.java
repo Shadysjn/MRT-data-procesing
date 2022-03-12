32d2a981fbc04616c78941277bb30345766af60f
AndrowAM <androw.abdelmalak@mail.mcgill.ca>
2022-03-12 15:06:36 -0500

    Optimised and added code to fix potential errors in the GuideController.java and RegisteredStepDefinitions.java

--- src/main/java/ca/mcgill/ecse/divesafe/controller/GuideController.java
+++ src/main/java/ca/mcgill/ecse/divesafe/controller/GuideController.java
@@ -28,8 +28,9 @@ public class GuideController {
 		int numberOfDots = getCharacterFrequency(email, '.');
 
 		//Verifies character counts
-		if(numberOfAts != 1 || numberOfDots != 1) return "Invalid email";
 		if(numberOfSpaces != 0) return "Email must not contain any spaces";
+		if(numberOfAts != 1 || numberOfDots != 1) return "Invalid email";
+
 
 		int positionOfAt = getCharacterPosition(email, '@');
 		int positionOfDot = getCharacterPosition(email, '.');
@@ -48,16 +49,16 @@ public class GuideController {
 		String[] splitEmailWithAt = email.split("@");
 
 		//Verifies position of '.' and '@' in email
-		if(splitEmailWithDot.length != 2 || positionOfDot == 0) return "Invalid email";
 		if(splitEmailWithAt.length != 2 || positionOfAt == 0) return "Invalid email";
-		if(positionOfDot < positionOfAt) return "Invalid email";
+		if(splitEmailWithDot.length != 2 || positionOfDot == 0) return "Invalid email";
 		if(Math.abs(positionOfDot - positionOfAt) == 1) return "Invalid email";
-
-		//check if the email is NOT linked to an existing guide account
-		if (Guide.hasWithEmail(email)) return "Email already linked to a guide account";
+		if(positionOfDot < positionOfAt) return "Invalid email";
 
 		//check if the email is NOT linked to an existing member account
 		if (Member.hasWithEmail(email)) return "Email already linked to a member account";
+
+		//check if the email is NOT linked to an existing guide account
+		if (Guide.hasWithEmail(email)) return "Email already linked to a guide account";
 
 		//Registering and adding the new guide
 		Guide newGuide = new Guide(email, password, name, emergencyContact, DiveSafeApplication.getDiveSafe());
--- src/test/java/ca/mcgill/ecse/divesafe/features/RegisterGuideStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/divesafe/features/RegisterGuideStepDefinitions.java
@@ -98,31 +98,31 @@ public class RegisterGuideStepDefinitions {
 
     /**
     * Verifies the existence of the guide
-    * @author AlecTufenkjian
+    * @author androwabdelmalak
     */
     @Then("a new guide account shall exist with {string}, {string}, {string}, and {string} \\(p3)")
     public void a_new_guide_account_shall_exist_with_and_p3(String email, String password, String name,String emergencyContact) {
-        List<Guide> guides = DiveSafeApplication.getDiveSafe().getGuides();
+        List<Guide> listOfGuides = DiveSafeApplication.getDiveSafe().getGuides();
 
-        boolean guideFound = false;
-        for (Guide guide: guides){
+        boolean foundGuide = false;
+
+        for (Guide guide: listOfGuides){
             if(guide.getEmail().equals(email) && guide.getPassword().equals(password) && guide.getName().equals(name) && guide.getEmergencyContact().equals(emergencyContact)) {
-                guideFound = true;
+                foundGuide = true;
+                break;
             }
         }
 
-        assertTrue(guideFound);
+        assertTrue(foundGuide);
     }
 
     /**
     * Compares the expected number of guides with the found number of guides
-    * @author AlecTufenkjian
+    * @author androwabdelmalak
     */
     @Then("the number of guides in the system is {int} \\(p3)")
-    public void the_number_of_guides_in_the_system_is_p3(Integer numberOfGuidesExpected) {
-        Integer numberOfGuidesFound = DiveSafeApplication.getDiveSafe().numberOfGuides();
-
-        assertEquals(numberOfGuidesExpected, numberOfGuidesFound);
+    public void the_number_of_guides_in_the_system_is_p3(Integer expectedNumberOfGuides) {
+        assertEquals(expectedNumberOfGuides, (Integer) DiveSafeApplication.getDiveSafe().numberOfGuides());
     }
 
     /**
